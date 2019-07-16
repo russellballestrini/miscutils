51012974586cea498dfcda1e486d3ef35808b0a8
russellballestrini <russell.ballestrini@gmail.com>
2019-07-16 08:06:44 -0400

    some helpers for dealing with values derived from a config .ini 	modified:   miscutils/__init__.py

--- miscutils/__init__.py
+++ miscutils/__init__.py
@@ -11,6 +11,53 @@ def generate_password(size=32):
     return "".join([choice(pool) for i in range(size)]).encode("utf-8")
 
 
+def get_int_or_bool_or_none_or_str(value):
+    """
+    Given a string value pulled from a configuration file,
+    this function attempts to return the value with the proper type.
+    """
+    try:
+        return int(value)
+    except ValueError:
+        if value.lower() in {"yes", "y", "true", "y"}:
+            return True
+        elif value.lower() in {"no", "n", "false", "f"}:
+            return False
+        elif value.lower() == "none":
+            return None
+        return str(value)
+
+
+def get_children_settings(settings, parent_key):
+    """
+    Accept a settings dict and parent key, return dict of children
+
+    For example:
+
+      auth_tkt.hashalg = md5
+
+    Results to:
+
+      {'auth_tkt.hashalg': 'md5'}
+
+    This function returns the following:
+
+      >>> get_children_settings({'auth_tkt.hashalg': 'md5'}, 'auth_tkt')
+      {'hashalg': 'md5'}
+
+    """
+    # the +1 is the . between parent and child settings.
+    parent_len = len(parent_key) + 1
+    children = {}
+    for key, value in settings.items():
+        if parent_key in key:
+            # expandvars replaces template with ENV vars.
+            children[key[parent_len:]] = get_int_or_bool_or_none_or_str(
+                expandvars(value)
+            )
+    return children
+
+
 def timestamp_to_datetime(timestamp):
     """Accepts a timestamp and returns a date string"""
     return datetime.fromtimestamp(timestamp / 1000.0)
