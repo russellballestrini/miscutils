8193d610a7709426eab15e916154460b55ccb3eb
russellballestrini <russell.ballestrini@gmail.com>
2020-01-19 23:52:28 -0500

    added a really dumb phone number validator. 	new file:   miscutils/phone_numbers.py 	modified:   setup.py

---
+++ miscutils/phone_numbers.py
@@ -0,0 +1,19 @@
+def is_phone_number_valid(phone_number):
+    """Test if user phone_number meets our criteria for validity."""
+    # Choices here are subject to change.
+    # allow dashes.
+    phone_number = phone_number.replace("-", "")
+    # allow period.
+    phone_number = phone_number.replace(".", "")
+    # allow spaces.
+    phone_number = phone_number.replace(" ", "")
+
+    # must be left with only digits.
+    if not phone_number.isdigit():
+        return False
+
+    # must be at least 10 digits.
+    if phone_number < 10:
+        return False
+
+    return True
