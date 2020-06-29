60ccd250b3444d9df43af398e28a17f034cc2f10
russellballestrini <russell.ballestrini@gmail.com>
2020-06-29 09:19:43 -0400

    bump version 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ def sanitize(x):
 with open("requirements.txt", "r") as f:
     requires = filter(sanitize, map(remove_whitespace, f.readlines()))
 
+
 setup(
     name="miscutils",
-    version="0.0.06",
+    version="0.0.07",
     description="Random misc lib utils for Python needed in lots of my projs",
     keywords="random misc lib utils for Python",
     # long_description=open("README.rst").read(),
